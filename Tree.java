5e2371b6b2e38125053d86817547c153f485e410
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2020-12-25 19:27:34 +0100

    Without baubles

--- Tree.java
+++ Tree.java
@@ -1,3 +1,5 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Tree {
@@ -11,9 +13,23 @@ public class Tree {
         return String.format("%" + n + "s", s);
     }
 
+    public String layer(int i) {
+        return IntStream.range(1, i)
+            .mapToObj(pos -> "x")
+            .collect(Collectors.joining(""));
+    }
+
+    public Stream<String> branches() {
+        return IntStream.range(1, size)
+            .mapToObj(i -> padLeft(layer(i), size)
+            + layer(i + 1)
+            );
+    }
+
     public Stream<String> get() {
-        return Stream.concat(Stream.of(
+        return Stream.concat(Stream.concat(Stream.of(
         padLeft("+", size + 1)),
+        branches()),
             Stream.of(padLeft("|||", size + 2)));
     }
 
