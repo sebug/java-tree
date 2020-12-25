ced166f7ad95d9f439a0ea99d17598ad6583138c
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2020-12-25 19:13:47 +0100

    Left padding top and bottom already

--- Tree.java
+++ Tree.java
@@ -7,9 +7,14 @@ public class Tree {
         this.size = size;
     }
 
+    private String padLeft(String s, int n) {
+        return String.format("%" + n + "s", s);
+    }
+
     public Stream<String> get() {
-        return Stream.concat(Stream.of("+"),
-            Stream.of("|||"));
+        return Stream.concat(Stream.of(
+        padLeft("+", size + 1)),
+            Stream.of(padLeft("|||", size + 2)));
     }
 
     public static void main(String[] args) {
